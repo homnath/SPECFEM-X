9a09ab9009bdbcbbbf174176b2fda60b3cd4592c
Etienne Bachmann <etienneb@tigergpu.princeton.edu>
2019-06-28 10:40:29 -0400

    Added stubs routines

---
+++ src/GPU_stubs.c
@@ -0,0 +1,20 @@
+#include "GPU.h"
+
+void FC_FUNC_(gpu_loop1,
+              GPU_LOOP1)(){}
+
+void FC_FUNC_(gpu_loop2,
+              GPU_LOOP2)(){}
+
+void FC_FUNC_(gpu_loop3,
+              GPU_LOOP3)(){}
+
+void FC_FUNC_(gpu_loop4,
+              GPU_LOOP4)(){}
+
+void FC_FUNC_(prepare_gpu,
+              PREPARE_GPU)(){}
+
+
+void FC_FUNC_(initialize_cuda_device,
+              INITIALIZE_CUDA_DEVICE)(){}
